56427d73d058130f519112eec8f8ab378ca549b5
ianwash <ianwash@live.unc.edu>
2022-04-10 18:19:29 -0400

    fixed logging

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
         useragent: req.headers['user-agent']
     }
     const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
-    const info = stmt.run(data.remoteaddr, data.remoteuser, data.time, data.method, data.url, data.protocol, data.httpversion, data.secure, data.status, data.referer, data.useragent);
+    const info = stmt.run(data.remoteaddr, data.remoteuser, data.time, data.method, data.url, data.protocol, data.httpversion, data.status, data.referer, data.useragent);
     // res.status(200).json(info);
     next();
 });
